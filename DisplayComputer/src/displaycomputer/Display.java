9d1dc3ce29b8ec196553f84f2f68eb73ad016ddb
sayedq <sayedq@sayed-pc>
2016-04-06 11:44:52 +0300

    Clean Start

---
+++ DisplayComputer/src/displaycomputer/Display.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package displaycomputer;
+
+/**
+ *
+ * @author sayedq
+ */
+import java.util.Scanner; //Import Java Class
+public class Display {
+    
+    public static void main(String[] args){
+        Scanner userinput = new Scanner(System.in); //Scanner onj
+        
+        
+        
+    }
+    
+}
